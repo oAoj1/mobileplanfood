567495042d883860e007a90e2f70009a76bf1cfd
oAoj1 <joaogbmarques@gmail.com>
2024-11-02 10:51:44 -0300

    atualizar a pagina ao inserir alimento novo

--- src/screens/InicioRefeicaoAgora/index.jsx
+++ src/screens/InicioRefeicaoAgora/index.jsx
@@ -1,69 +1,83 @@
-import { useEffect, useState } from "react";
-
+import { useEffect, useState, useCallback } from "react";
 import { 
   SafeAreaView, 
   Text, 
   View, 
   FlatList,
   TouchableOpacity,
-  StyleSheet 
+  StyleSheet,
+  RefreshControl
 } from "react-native";
 
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
 
-import api from "../../api/api"
+import api from "../../api/api";
 
-import IoniIcons from 'react-native-vector-icons/Ionicons'
+import IoniIcons from 'react-native-vector-icons/Ionicons';
 
-import Header from "../../components/Header"
+import Header from "../../components/Header";
 
-export default function RefeicaoAgora(){
+export default function RefeicaoAgora() {
 
-  const [refeicaoAgora,setRefeicaoAgora] = useState([])
+  const [refeicaoAgora, setRefeicaoAgora] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [novoAlimentoInserido, setNovoAlimentoInserido] = useState(false);
+  const navigation = useNavigation();
+
+  const lerRefeicaoAgora = async () => {
+    try {
+      const response = await api.get('/refeicoes/agora');
+      const data = response.data;
+      setRefeicaoAgora([data]);
+      setNovoAlimentoInserido(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    lerRefeicaoAgora().then(() => setRefreshing(false));
+  }, []);
 
   useEffect(() => {
-    async function lerRefeicaoAgora(){
-      try{
-        const response = await api.get('/refeicoes/agora')
-        const data = response.data
+    lerRefeicaoAgora();
+  }, []);
 
-        setRefeicaoAgora([data])
-      }catch(error){
-        console.log(error)
-      }
-    }
-
-    lerRefeicaoAgora()
-  },[])
-
-  const navigation = useNavigation()
-
-  function adicionarAlimento(){
-      navigation.navigate('AdicionarAlimento')
+  function adicionarAlimento() {
+    navigation.navigate('AdicionarAlimento');
+    setNovoAlimentoInserido(true);
   }
 
-  async function removerAlimento(idRefeicao,idAlimento){
+  async function removerAlimento(idRefeicao, idAlimento) {
     try {
-      await api.delete(`/refeicoes/${idRefeicao}/alimentos/${idAlimento}`)
-      alert('alimento deletado')
-      
+      await api.delete(`/refeicoes/${idRefeicao}/alimentos/${idAlimento}`);
+      alert('Alimento deletado');
+      lerRefeicaoAgora(); 
     } catch (error) {
-      alert('erro ao deletar alimento')
-      console.log(error)
+      alert('Erro ao deletar alimento');
+      console.log(error);
     }
-    
   }
 
-  return(
+  return (
     <SafeAreaView style={styles.telaPrincipal}> 
-
-      <Header/>
+      <Header />
+      
+      {novoAlimentoInserido && (
+        <TouchableOpacity style={styles.novoAlimentoBanner} onPress={onRefresh}>
+          <Text style={styles.novoAlimentoTexto}>Novo alimento inserido, puxe para baixo para atualizar</Text>
+        </TouchableOpacity>
+      )}
 
       <FlatList
         data={refeicaoAgora}
         keyExtractor={item => item._id}
-        renderItem={({item:refeicao}) => (
-          <View style={styles.refeicaoAgoraContaner}>
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        renderItem={({ item: refeicao }) => (
+          <View style={styles.refeicaoAgoraContainer}>
             <Text style={styles.dia}>
               {refeicao.dia}
             </Text>
@@ -75,25 +89,23 @@ export default function RefeicaoAgora(){
               <FlatList
                 data={refeicao.alimentos}
                 keyExtractor={item => item._id}
-                renderItem={({item:alimento}) => (
-                    <View style={styles.alimentoContainer}>
-                      <Text style={styles.alimento}>
-                        {alimento.alimento}
-                      </Text>
-
-                      <TouchableOpacity 
-                        onPress={() => removerAlimento(refeicao._id,alimento._id)}
-                      >
-                        <IoniIcons
-                          name='close-outline'
-                          size={25}
-                          color='#424242'
-                        />
-                      </TouchableOpacity>
-                    </View>
+                renderItem={({ item: alimento }) => (
+                  <View style={styles.alimentoContainer}>
+                    <Text style={styles.alimento}>
+                      {alimento.alimento}
+                    </Text>
+                    <TouchableOpacity 
+                      onPress={() => removerAlimento(refeicao._id, alimento._id)}
+                    >
+                      <IoniIcons
+                        name='close-outline'
+                        size={25}
+                        color='#424242'
+                      />
+                    </TouchableOpacity>
+                  </View>
                 )}
               />
-
             </View>
           </View>
         )}
@@ -109,74 +121,81 @@ export default function RefeicaoAgora(){
       </TouchableOpacity>
 
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  telaPrincipal:{
-    
-  },  
-  tituloHeader:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    padding:10,
+  telaPrincipal: {},
+  tituloHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
   },
-  planFoodText:{
-    fontSize:14,
-    color:'#424242',
-    marginRight:25
+  planFoodText: {
+    fontSize: 14,
+    color: '#424242',
+    marginRight: 25,
   },
-  refeicaoAgoraContaner:{
-    marginTop:10,
-    alignItems:'center'
+  refeicaoAgoraContainer: {
+    marginTop: 10,
+    alignItems: 'center',
   },
-  dia:{
-    fontSize:20,
-    textTransform:'capitalize',
-    color:'#272727'
+  dia: {
+    fontSize: 20,
+    textTransform: 'capitalize',
+    color: '#272727',
   },
-  refeicao:{
-    fontWeight:'bold',
-    fontSize:50,
-    textTransform:'capitalize',
-    marginBottom:10,
-    color:'#000000'
+  refeicao: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    textTransform: 'capitalize',
+    marginBottom: 10,
+    color: '#000000',
   },
-  listaAlimentosContainer:{
-    width:410,
-    height:500,
-    backgroundColor:'#36F76D',
-    borderTopLeftRadius:40,
-    borderTopRightRadius:40,
-    alignItems:'center',
-    paddingTop:10,
-    elevation:-5
+  listaAlimentosContainer: {
+    width: 410,
+    height: 500,
+    backgroundColor: '#36F76D',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    alignItems: 'center',
+    paddingTop: 10,
+    elevation: -5,
   },
-  alimentoContainer:{
-    flexDirection:'row',
-    alignItems:'center',
-    backgroundColor:'#85F9A0',
-    justifyContent:'space-between',
-    borderRadius:20,
-    width:350,
-    marginVertical:5,
-    padding:7,
-    elevation:5
-  },  
-  alimento:{
-    textTransform:'capitalize',
-    fontSize:20,
-    paddingLeft:18,
-    fontWeight:'regular',
-    color:'#000000'
+  alimentoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#85F9A0',
+    justifyContent: 'space-between',
+    borderRadius: 20,
+    width: 350,
+    marginVertical: 5,
+    padding: 7,
+    elevation: 5,
   },
-  iconAdd:{
-    position:'absolute',
-    bottom:18,
-    left:'42.10%',
-    backgroundColor:'#000',
-    padding:7,
-    borderRadius:50,
-    elevation:10
+  alimento: {
+    textTransform: 'capitalize',
+    fontSize: 20,
+    paddingLeft: 18,
+    fontWeight: 'regular',
+    color: '#000000',
+  },
+  iconAdd: {
+    position: 'absolute',
+    bottom: 18,
+    left: '42.10%',
+    backgroundColor: '#000',
+    padding: 7,
+    borderRadius: 50,
+    elevation: 10,
+  },
+  novoAlimentoBanner: {
+    backgroundColor: '#ffdd57',
+    padding: 10,
+    alignItems: 'center',
+  },
+  novoAlimentoTexto: {
+    color: '#000',
+    fontWeight: 'bold',
   }
-})
+});
